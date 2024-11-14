13cdcea791c58484fa73ffb4b891562bf61f12c7
folli14 <cesarfolli@gmail.com>
2024-11-13 23:54:06 -0300

    criação da NavBar e implementação do drawer

---
+++ src/components/layouts/NavBar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import Drawer from "../Drawer";
+import searchIcon from "@/icons/search_icon.svg";
+import { FaSquarePlus } from "react-icons/fa6";
+import CardCriacao from "../create/MedicalForm";
+
+interface navBarProps {
+  name: string;
+}
+
+const NavBar: React.FC<navBarProps> = ({ name }) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [criar, setCriar] = useState(false);
+
+  const openDrawer = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+
+  const criarProntuario = () => {
+    setCriar(true);
+  };
+  const fecharProntuario = () => {
+    setCriar(false);
+  };
+
+  return (
+    <nav className="w-full h-28 flex flex-row bg-[#F1FFFC]">
+      <div className="w-1/2 h-full flex">
+        <div className="w-1/4 h-full flex justify-center items-center">
+          <button onClick={openDrawer}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/7216/7216128.png"
+              className="h-10"
+              alt=""
+            />
+          </button>
+        </div>
+        <Drawer isOpen={isDrawerOpen} onClose={closeDrawer} />
+        <div className="w-3/4 h-full flex items-center gap-2">
+          <Link href="/">
+            <h1
+              className={`font-bold text-5xl text-[#114238] `}
+            >
+              {name}
+            </h1>
+          </Link>
+          <div className="border-t border-2 border-[#1F6657] w-44" />
+        </div>
+      </div>
+      <div className="w-1/2 h-full flex items-center justify-between">
+        <button
+          className="bg-[#A3D6CB] w-60 h-11 rounded-full flex justify-between p-2 items-center border-2 border-black"
+          onClick={criarProntuario}
+        >
+          <p className="text-xl font-medium text-[#114238]">
+            Criar novo prontuário
+          </p>
+          <FaSquarePlus color="#114238" size={18} />
+        </button>
+        <CardCriacao isOpen={criar} onClose={fecharProntuario} />
+        <div className="m-10 flex items-center border border-black rounded-full w-[25rem] overflow-hidden">
+          <div className="p-2">
+            <Image
+              alt="search icon"
+              src={searchIcon}
+              width={35}
+              height={35}
+              className="text-[#114238]"
+            />
+          </div>
+          <input
+            type="text"
+            placeholder={`Pesquisar ${name}...`}
+            className={`w-full p-2 text-center outline-none text-[#114238] bg-transparent`}
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
