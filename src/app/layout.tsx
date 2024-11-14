5ba77996bc169f69a2d6584236a6e93759b754da
folli14 <cesarfolli@gmail.com>
2024-11-13 23:50:38 -0300

    adição das fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,28 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import { Fira_Sans_Extra_Condensed } from "next/font/google";
+
 
 export const metadata: Metadata = {
   title: "Kalos",
   description: "",
 };
+
+
+const fira_medium = Fira_Sans_Extra_Condensed({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "500",
+  style: "normal"
+});
+
+const fira_regular = Fira_Sans_Extra_Condensed({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  style: "normal"
+});
+
 
 export default function RootLayout({
   children,
@@ -13,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
+      <body className={`${fira_medium.className} ${fira_regular.className}   bg-[#F1FFFC]`}>
         {children}
       </body>
     </html> 
