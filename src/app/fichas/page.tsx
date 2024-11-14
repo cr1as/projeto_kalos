a1cc6aefcaf1cd9ce7e471a87dba74c8824739d6
folli14 <cesarfolli@gmail.com>
2024-11-13 23:52:15 -0300

    adição dos card (fichas)

--- src/app/fichas/page.tsx
+++ src/app/fichas/page.tsx
@@ -1,20 +1,67 @@
-import MainLayout from "@/components/MainLayout";
-import { registros } from "@/mock/registros";
-import Ficha from "@/components/Ficha";
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
 
+import MainLayout from "@/components/layouts/MainLayout";
+import { pacientes } from "@/mock/registros";
+import Preview from "@/components/preview/PreviewFichas";
+import { useEffect, useState } from "react";
+
+interface Paciente {
+  nome: string;
+  idade: string;
+  genero: string;
+  telefone: string;
+  peso: string;
+  altura: string;
+  especificacoesAdicionais?: string;
+  rua: string;
+  numero: string;
+  cidade: string;
+  bairro: string;
+  tipo: string;
+  ativo?: boolean;
+}
 
 const page: React.FC = () => {
-    return (
-        <MainLayout nav="Ficha Médica" >
-            <div className="w-full flex justify-center">
+  
+  const [prontuarios, setProntuarios] = useState<Paciente[]>([]);
 
-            <div className="grid grid-cols-2 gap-10 justify-items-center">
-            {registros.map((registro) => (
-                <Ficha key={registro.nome_paciente} tipo={registro.Tipo} nome_paciente={registro.nome_paciente} data_nascimento={registro.data_nascimento} cpf={registro.cpf} rg={registro.rg}/>
-            ))}
-            </div>
-            </div>
-        </MainLayout>
-    )
-}
+
+  useEffect(() => {
+    setProntuarios(
+      pacientes.filter(registro => registro.tipo === "Ficha Médica")
+    );
+  }, []);
+  
+
+  return (
+    <MainLayout nav="Fichas Médica">
+      <div className="w-full flex justify-center">
+        <div className="grid grid-cols-2 gap-10 justify-items-center">
+          {prontuarios.map(
+            (prontuario) => {
+              return (
+                <Preview
+                  key={prontuario.nome}
+                  tipo={prontuario.tipo}
+                  nome={prontuario.nome}
+                  genero={prontuario.genero}
+                  idade={prontuario.idade}
+                  altura={prontuario.altura}
+                  peso={prontuario.peso}
+                  telefone={prontuario.telefone}
+                  cidade={prontuario.cidade}
+                  bairro={prontuario.bairro}
+                  rua={prontuario.rua}
+                  numero={prontuario.numero}
+                  especificacoesAdicionais={prontuario.especificacoesAdicionais}
+                />
+              );
+            }
+          )}
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
 export default page;
