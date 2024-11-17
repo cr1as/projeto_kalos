6425b26323619a5db40693a7c749f45666ba3e07
follijulio <follijulio@gmail.com>
2024-11-17 13:46:43 -0300

    adição do banco de dados funcionarios

---
+++ src/mock/funcionarios.ts
@@ -0,0 +1,72 @@
+const funcionarios = [
+    {
+      nome: "Carlos Silva",
+      usuario: "carlos123",
+      senha: "senhaCarlos1",
+      codigos: [111, 222, 333, 444, 555, 666]
+    },
+    {
+      nome: "Ana Pereira",
+      usuario: "ana.pereira",
+      senha: "senhaAna123",
+      codigos: [789, 654, 321, 987, 555, 666]
+    },
+    {
+      nome: "Lucas Oliveira",
+      usuario: "lucas_oliveira",
+      senha: "senhaLucas2024",
+      codigos: [102, 203, 304, 405, 506, 607]
+    },
+    {
+      nome: "Mariana Costa",
+      usuario: "mariana_costa",
+      senha: "senhaMari2024",
+      codigos: [501, 602, 703, 804, 905, 106]
+    },
+    {
+      nome: "José Santos",
+      usuario: "jose_santos",
+      senha: "senhaJose2024",
+      codigos: [112, 223, 334, 445, 556, 667]
+    },
+    {
+      nome: "Fernanda Lima",
+      usuario: "fernanda.lima",
+      senha: "senhaFernanda1",
+      codigos: [800, 900, 1000, 1100, 1200, 1300]
+    },
+    {
+      nome: "Gustavo Almeida",
+      usuario: "gustavo_almeida",
+      senha: "senhaGustavo12",
+      codigos: [111, 213, 312, 415, 512, 615]
+    },
+    {
+      nome: "Patrícia Martins",
+      usuario: "patricia_martins",
+      senha: "senhaPatricia123",
+      codigos: [101, 202, 303, 404, 505, 606]
+    },
+    {
+      nome: "Ricardo Souza",
+      usuario: "ricardo.souza",
+      senha: "senhaRicardo2024",
+      codigos: [1001, 2002, 3003, 4004, 5005, 6006]
+    },
+    {
+      nome: "Juliana Rocha",
+      usuario: "juliana_rocha",
+      senha: "senhaJuli2024",
+      codigos: [601, 702, 803, 904, 105, 206]
+    },
+    {
+        nome: "folli",
+        usuario: "folli",
+        senha: "1423",
+        codigos: [111, 222, 333, 444, 555, 1423]
+    }
+
+  ];
+  
+  export { funcionarios };
+  
